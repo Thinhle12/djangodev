13f6f44f230affcb2d8fe7b4ee94a46a275374fa
Thinhle12 <hungthinh12699@gmail.com>
2022-06-08 06:42:13 +0700

    first commit

---
+++ Scripts/mysite/hello/views.py
@@ -0,0 +1,15 @@
+import django
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "hello/index.html")
+def brain(request):
+    return HttpResponse("hello brain")
+def david(request):
+    return HttpResponse("hello david")
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name":name.capitalize()
+    })
